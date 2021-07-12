e8beae8bc5708ffcee5a45d602e5e92b29c8d5fd
Tong Thien Viet Anh <anh.tong@elca.vn>
2021-07-12 22:30:57 +0700

    add delete employee endpoint

--- backend/src/main/java/com/elca/backend/controller/EmployeeController.java
+++ backend/src/main/java/com/elca/backend/controller/EmployeeController.java
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,6 +56,13 @@ public class EmployeeController {
             throws EmployeeVisaAlreadyExistsException {
         employeeService.createEmployee(employeeDto);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Delete employee")
+    public ResponseEntity<Void> deleteEmployee(@PathVariable Long id) {
+        employeeRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
