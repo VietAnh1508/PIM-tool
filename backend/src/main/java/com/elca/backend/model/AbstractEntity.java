b88aa7ffaa13df3b40c32bfe302672dee43f851a
Tong Thien Viet Anh <anh.tong@elca.vn>
2021-07-12 21:55:06 +0700

    add getter setter to expose id property to json response

--- backend/src/main/java/com/elca/backend/model/AbstractEntity.java
+++ backend/src/main/java/com/elca/backend/model/AbstractEntity.java
@@ -1,22 +1,28 @@
 package com.elca.backend.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
-@MappedSuperclass
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@MappedSuperclass
 public abstract class AbstractEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @JsonIgnore
     private Integer version;
 
 }
